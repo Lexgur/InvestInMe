fb638ddd6b7905c455c70517e75544afdd05402d
Lexgur <edgaras.malukas@gmail.com>
2025-05-01 21:41:20 +0300

    Loader isntead of Loading...

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,11 +1,12 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from './AuthContext';
+import Loader from './Loader';
 
 const ProtectedRoute = () => {
   const { user, loading } = useAuth();
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div><Loader /></div>;
   }
 
   return user ? <Outlet /> : <Navigate to="/login" replace />;
